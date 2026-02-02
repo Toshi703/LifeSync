56823a1665045b3ba5639f9f18fafdb26dbdece0
Toshi703 <pavloskaya2010@gmail.com>
2026-02-02 20:45:04 +0100

    Refactor energyLog and related functions

--- modules/stats.js
+++ modules/stats.js
@@ -1,10 +1,10 @@
-export let energyLog = [80]; // Historique initial
+export let energyLog = [80];
 
-export function logEnergy(value) {
+export function logEnergy(value){
   energyLog.push(Number(value));
-  if (energyLog.length > 20) energyLog.shift(); // garder les 20 derniers points
+  if(energyLog.length>20) energyLog.shift();
 }
 
-export function getEnergyLog() {
+export function getEnergyLog(){
   return [...energyLog];
 }
